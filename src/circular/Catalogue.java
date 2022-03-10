628f7347b6c144a6a2bdaf2ec4f879471d3cfb86
DizzyMoon <mikkel.simons1@gmail.com>
2022-03-10 11:05:15 +0100

    All methods are now functional

--- src/circular/Catalogue.java
+++ src/circular/Catalogue.java
@@ -1,69 +1,75 @@
 package circular;
 
 public class Catalogue {
-    private Item[] itemList;
+  private Item[] itemList;
 
-    public Catalogue(int catalogueSize) {
-        this.itemList = new Item[catalogueSize];
-    }
+  public Catalogue(int catalogueSize) {
+    this.itemList = new Item[catalogueSize];
+  }
 
-    public int arrayIndexOf(Item[] arrayIn, Item input) {
-        for (int i = 0; i < arrayIn.length; i++) {
-            if (arrayIn[i] == input) {
-                return i;
-            }
-        }
-        return -1;
+  public int arrayIndexOf(Item[] arrayIn, Item input) {
+    for (int i = 0; i < arrayIn.length; i++) {
+      if (arrayIn[i] == input) {
+        return i;
+      }
     }
+    return -1;
+  }
 
-    public int arrayIndexOf(String[] arrayIn, String input) {
-        for (int i = 0; i < arrayIn.length; i++) {
-            if (arrayIn[i] == input) {
-                return i;
-            }
-        }
-        return -1;
+  public int arrayIndexOf(String[] arrayIn, String input) {
+    for (int i = 0; i < arrayIn.length; i++) {
+      if (arrayIn[i] == input) {
+        return i;
+      }
     }
+    return -1;
+  }
 
-    public Item[] getFullList() {
-        return this.itemList;
-    }
+  public Item[] getFullList() {
 
-    public Item findItem(String input) {
-        for (int i = 0; i < this.itemList.length; i++) {
-            if (this.itemList[i].getDescription().equals(input)) {
-                return this.itemList[i];
-            }
-        } return null;
-    }
+    return this.itemList;
+  }
 
-    public Item[] getAvailableItems(){
-        Item[] availableItems = new Item[0];
-        int j = 0;
-            for (int i = 0; i <= this.itemList.length; i++){
-            if (this.itemList[i].getAvailable()) {
-                j++;
-            }
-        }
-        for (int i = 0; i <= this.itemList.length; i++){
-            availableItems = new Item[j];
-            if (this.itemList[i].getAvailable() && this.itemList[i] != null){
-                int k = 0;
-                availableItems[k] = this.itemList[i];
-                k++;
-            }
-        } return availableItems;
+  public Item findItem(String input) {
+    for (int i = 0; i < this.itemList.length; i++) {
+      if (this.itemList[i].getDescription().equals(input)) {
+        return this.itemList[i];
+      }
     }
+    return null;
+  }
 
-    public void borrowItem(Item input){
-        input.setAvailable(false);
+  public Item[] getAvailableItems() {
+    Item[] availableItems = new Item[0];
+    int j = 0;
+    int k = 0;
+    for (int i = 0; i < this.itemList.length; i++) {
+      if (this.itemList[i] != null && this.itemList[i].getAvailable()) {
+        j++;
+      } else if (this.itemList[i] == null) {
+        this.itemList[i] = new Item("Empty", "Empty");
+        this.itemList[i].setAvailable(false);
+      }
     }
+    availableItems = new Item[j];
+    for (int i = 0; i < this.itemList.length; i++) {
+      if (this.itemList[i] != null && this.itemList[i].getAvailable()) {
+        availableItems[k] = this.itemList[i];
+        k++;
+      }
+    }
+    return availableItems;
+  }
 
-    public void returnItem(Item input){
-        input.setAvailable(true);
-    }
+  public void borrowItem(Item input) {
+    input.setAvailable(false);
+  }
 
-    public void addItem (Item input){
-        this.itemList[arrayIndexOf(this.itemList, null)] = input;
-    }
+  public void returnItem(Item input) {
+    input.setAvailable(true);
+  }
+
+  public void addItem(Item input) {
+    this.itemList[arrayIndexOf(this.itemList, null)] = input;
+  }
 }
